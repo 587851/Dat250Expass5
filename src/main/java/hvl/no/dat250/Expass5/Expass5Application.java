55eaabf6725fd6b36a6cc4522d923b6d79078700
587851 <587851@stud.hvl.no>
2023-10-02 11:49:11 +0200

    ferdig

---
+++ src/main/java/hvl/no/dat250/Expass5/Expass5Application.java
@@ -0,0 +1,97 @@
+package hvl.no.dat250.Expass5;
+
+import hvl.no.dat250.Expass5.accesingdatajpa.Customer;
+import hvl.no.dat250.Expass5.accesingdatajpa.CustomerRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Arrays;
+
+@SpringBootApplication
+@RestController
+@EnableJpaRepositories(basePackageClasses={CustomerRepository.class})
+public class Expass5Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Expass5Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Expass5Application.class, args);
+	}
+
+	/**
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+	**/
+
+	/**
+	 * @GetMapping("/") public String index() {
+	 * return "Greetings from Spring Boot!";
+	 * }
+	 **/
+
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			// save a few customers
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+
+			// fetch an individual customer by ID
+			Customer customer = repository.findById(1L);
+			log.info("Customer found with findById(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+			log.info("");
+
+			// fetch customers by last name
+			log.info("Customer found with findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			repository.findByLastName("Bauer").forEach(bauer -> {
+				log.info(bauer.toString());
+			});
+			// for (Customer bauer : repository.findByLastName("Bauer")) {
+			//  log.info(bauer.toString());
+			// }
+			log.info("");
+		};
+
+	}
+}
